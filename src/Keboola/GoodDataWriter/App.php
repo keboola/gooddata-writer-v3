acbc14d6bf21236a5ea3e1a57667e5ecc1d299bf
Jakub Matejka <jakub@keboola.com>
2017-09-20 16:08:03 +0200

    :cop: Improve code

--- src/Keboola/GoodDataWriter/App.php
+++ src/Keboola/GoodDataWriter/App.php
@@ -9,7 +9,6 @@ namespace Keboola\GoodDataWriter;
 use Keboola\GoodData\Client;
 use Keboola\GoodData\Datasets;
 use Keboola\GoodData\Exception;
-use Keboola\GoodData\Identifiers;
 use Keboola\GoodData\WebDav;
 use Keboola\Temp\Temp;
 use Monolog\Handler\StreamHandler;
@@ -23,74 +22,45 @@ class App
     /** @var  ConsoleOutput */
     private $consoleOutput;
 
+    /** @var  Client */
+    protected $gdClient;
+
+    /** @var  Temp */
+    protected $temp;
+
+    /** @var  Logger */
+    protected $logger;
+
     public function __construct($consoleOutput)
     {
         $this->consoleOutput = $consoleOutput;
+        $this->logger = new Logger('gooddata-writer', [new StreamHandler('php://stdout')]);
+        $this->temp = new Temp();
+        $this->temp->initRunFolder();
     }
 
     public function run($config, $inputPath)
     {
-        if (!isset($config['parameters']['user']['login'])) {
-            throw new UserException('User login is missing from configuration');
-        }
-        if (!isset($config['parameters']['user']['#password'])) {
-            throw new UserException('User password is missing from configuration');
-        }
-        if (!isset($config['parameters']['project']['pid'])) {
-            throw new UserException('Project pid is missing from configuration');
-        }
+        $this->checkConfig($config);
         $pid = $config['parameters']['project']['pid'];
 
-        if (!isset($config['storage']['input']['tables']) || !count($config['storage']['input']['tables'])) {
-            throw new UserException('There are no tables on input');
-        }
-        if (!isset($config['parameters']['tables']) || !count($config['parameters']['tables'])) {
-            throw new UserException('There are no configured tables');
-        }
-
-        $logger = new Logger('gooddata-writer', [new StreamHandler('php://stdout')]);
-        $gdClient = $this->initGoodDataClient($config);
-        $gdClient->setLogger($logger);
-        $temp = new Temp();
-        $temp->initRunFolder();
-
+        $this->gdClient = $this->initGoodDataClient($config);
+        $this->gdClient->setLogger($this->logger);
 
         // Date dimensions
         if (isset($config['parameters']['dimensions']) && count($config['parameters']['dimensions'])) {
             foreach ($config['parameters']['dimensions'] as $dimensionName => $dimension) {
-                $identifier = !empty($dimension['identifier']) ? $dimension['identifier']
-                    : $gdClient->getDateDimensions()->getDefaultIdentifier($dimensionName);
-                $template = !empty($dimension['template']) ? $dimension['template'] : null;
-                if (!$gdClient->getDateDimensions()->exists($pid, $dimensionName, $template)) {
-                    $gdClient->getDateDimensions()->executeCreateMaql($pid, $dimensionName, $identifier, $template);
-                }
-                if (!empty($dimension['includeTime'])) {
-                    $tmpDir = $temp->getTmpFolder() . '/' . $identifier;
-                    mkdir($tmpDir);
-                    $td = new \Keboola\GoodData\TimeDimension($gdClient);
-                    if (!$td->exists($pid, $dimensionName, $identifier)) {
-                        $td->executeCreateMaql($pid, $dimensionName, $identifier);
-                        $td->loadData($pid, $dimensionName, $tmpDir);
-                    }
-                }
+                $this->createDateDimension($pid, $dimensionName, $dimension);
             }
         }
 
+        // Update model
         $projectDefinition = [
             'dataSets' => $config['parameters']['tables'],
             'dimensions' => $config['parameters']['dimensions']
         ];
-        $model = Model::getProjectLDM($projectDefinition);
-        $gdClient->getProjectModel()->updateProject($pid, $model);
-
-        $folderName = 'kbc-' . date('Ymd-his');
-        $webDav = new WebDav(
-            $config['parameters']['user']['login'],
-            $config['parameters']['user']['#password'],
-            $gdClient->getUserUploadUrl()
-        );
-        $webDav->createFolder($folderName);
-        $csvHandler = new CsvHandler($logger);
+        $projectModel = Model::getProjectLDM($projectDefinition);
+        $this->gdClient->getProjectModel()->updateProject($pid, $projectModel);
 
         $tables = $config['parameters']['tables'];
         foreach ($config['storage']['input']['tables'] as $table) {
@@ -105,77 +75,134 @@ class App
             $tableDefinition = Model::enhanceDefinition($tableId, $tables[$tableId], $projectDefinition);
 
             $filesToUpload = [];
-            $tmpDir = $temp->getTmpFolder() . '/' . $tableId;
+            $tmpDir = $this->temp->getTmpFolder() . '/' . $tableId;
             mkdir($tmpDir);
 
-            // Manifest
-            $manifest = Datasets::getDataLoadManifest(
-                $tableDefinition['identifier'],
-                $tableDefinition['columns'],
-                !empty($tableDefinition['incremental'])
-            );
-            $manifestFile = "$tmpDir/upload_info.json";
-            file_put_contents($manifestFile, json_encode($manifest));
-            $filesToUpload[] = $manifestFile;
+            $filesToUpload[] = $this->createManifest($tableDefinition, $tmpDir);
+            $filesToUpload[] = $this->createCsv("$inputPath/{$table['destination']}", $tableDefinition, $tmpDir);
 
-            // Prepare csv
-            $csvHandler->convert(
-                "$inputPath/{$table['destination']}",
-                "$tmpDir/{$tableDefinition['identifier']}.csv",
-                $tableDefinition['columns']
-            );
-            $filesToUpload[] = "$tmpDir/{$tableDefinition['identifier']}.csv";
+            $this->uploadFiles($config, $filesToUpload, $tableId, $tmpDir);
+        }
+    }
 
-            // Upload
-            // Retry three times if necessary (sometimes it fails on GD maintenance)
-            $repeat = 1;
-            $uploadUrl = $webDav->getUrl() . $folderName . '/upload.zip';
-            do {
-                $webDav->uploadZip($filesToUpload, $webDav->getUrl() . $folderName);
-                if ($webDav->fileExists($uploadUrl)) {
-                    $logger->debug("Upload package for table $tableId transferred to GoodData");
-                    break;
-                }
-                if ($repeat >= 5) {
-                    throw new UserException("Transfer package for table $tableId has not been uploaded to '$uploadUrl'");
-                }
-                $repeat++;
-                $logger->warn("Transfer of package for table $tableId to GoodData failed, running try #{$repeat}");
-            } while ($repeat <= 5);
+    public function checkConfig($config)
+    {
+        if (!isset($config['parameters']['user']['login'])) {
+            throw new UserException('User login is missing from configuration');
+        }
+        if (!isset($config['parameters']['user']['#password'])) {
+            throw new UserException('User password is missing from configuration');
+        }
+        if (!isset($config['parameters']['project']['pid'])) {
+            throw new UserException('Project pid is missing from configuration');
+        }
+        if (!isset($config['storage']['input']['tables']) || !count($config['storage']['input']['tables'])) {
+            throw new UserException('There are no tables on input');
+        }
+        if (!isset($config['parameters']['tables']) || !count($config['parameters']['tables'])) {
+            throw new UserException('There are no configured tables');
+        }
+    }
 
-            // Run ETL task
-            try {
-                $gdClient->getDatasets()->loadData($pid, $folderName);
-            } catch (Exception $e) {
-                $debugFile = "$tmpDir/etl.log";
-                $logSaved = $webDav->saveLogs($folderName, $debugFile);
-                if ($logSaved) {
-                    $logger->err('Data load error: ' . file_get_contents($debugFile));
-                }
-
-                $data = $e->getData();
-                if (!empty($data['error']['message'])) {
-                    $data['error']['message'] = str_replace(
-                        'Adjust the manifest or create the object.',
-                        'Have you updated dataset\'s model after changes?',
-                        $data['error']['message']
-                    );
-                    $e = new Exception($data['error']['message'], $e->getCode(), $e);
-                }
-
-                throw $e;
+    public function createDateDimension($pid, $dimensionName, $def)
+    {
+        $identifier = !empty($def['identifier']) ? $def['identifier']
+            : $this->gdClient->getDateDimensions()->getDefaultIdentifier($dimensionName);
+        $template = !empty($def['template']) ? $def['template'] : null;
+        if (!$this->gdClient->getDateDimensions()->exists($pid, $dimensionName, $template)) {
+            $this->gdClient->getDateDimensions()->executeCreateMaql($pid, $dimensionName, $identifier, $template);
+        }
+        if (!empty($def['includeTime'])) {
+            $tmpDir = $this->temp->getTmpFolder() . '/' . $identifier;
+            mkdir($tmpDir);
+            $td = new \Keboola\GoodData\TimeDimension($this->gdClient);
+            if (!$td->exists($pid, $dimensionName, $identifier)) {
+                $td->executeCreateMaql($pid, $dimensionName, $identifier);
+                $td->loadData($pid, $dimensionName, $tmpDir);
             }
+        }
+    }
+
+    public function createManifest($tableDefinition, $tmpDir)
+    {
+        $manifest = Datasets::getDataLoadManifest(
+            $tableDefinition['identifier'],
+            $tableDefinition['columns'],
+            !empty($tableDefinition['incremental'])
+        );
+        $manifestFile = "$tmpDir/upload_info.json";
+        file_put_contents($manifestFile, json_encode($manifest));
+        return $manifestFile;
+    }
+
+    public function createCsv($inputFile, $tableDefinition, $tmpDir)
+    {
+        $csvHandler = new CsvHandler($this->logger);
+        $csvHandler->convert(
+            $inputFile,
+            "$tmpDir/{$tableDefinition['identifier']}.csv",
+            $tableDefinition['columns']
+        );
+        return "$tmpDir/{$tableDefinition['identifier']}.csv";
+    }
+
+    public function uploadFiles($config, $filesToUpload, $tableId, $tmpDir)
+    {
+        $folderName = 'kbc-' . date('Ymd-his');
+        $webDav = new WebDav(
+            $config['parameters']['user']['login'],
+            $config['parameters']['user']['#password'],
+            $this->gdClient->getUserUploadUrl()
+        );
+        $webDav->createFolder($folderName);
+
+        // Retry three times if necessary (sometimes it fails on GD maintenance)
+        $repeat = 1;
+        $uploadUrl = $webDav->getUrl() . $folderName . '/upload.zip';
+        do {
+            $webDav->uploadZip($filesToUpload, $webDav->getUrl() . $folderName);
+            if ($webDav->fileExists($uploadUrl)) {
+                $this->logger->debug("Upload package for table $tableId transferred to GoodData");
+                break;
+            }
+            if ($repeat >= 5) {
+                throw new UserException("Transfer package for table $tableId has not been uploaded to '$uploadUrl'");
+            }
+            $repeat++;
+            $this->logger->warn("Transfer of package for table $tableId to GoodData failed, running try #{$repeat}");
+        } while ($repeat <= 5);
+
+        try {
+            $this->gdClient->getDatasets()->loadData($config['parameters']['project']['pid'], $folderName);
+        } catch (Exception $e) {
+            $debugFile = "$tmpDir/etl.log";
+            $logSaved = $webDav->saveLogs($folderName, $debugFile);
+            if ($logSaved) {
+                $this->logger->err('Data load error: ' . file_get_contents($debugFile));
+            }
+
+            $data = $e->getData();
+            if (!empty($data['error']['message'])) {
+                $data['error']['message'] = str_replace(
+                    'Adjust the manifest or create the object.',
+                    'Have you updated dataset\'s model after changes?',
+                    $data['error']['message']
+                );
+                $e = new Exception($data['error']['message'], $e->getCode(), $e);
+            }
+
+            throw $e;
         }
     }
 
     protected function initGoodDataClient($config)
     {
-        $gdClient = new Client();
-        $gdClient->setUserAgent('gooddata-writer-v3', getenv('KBC_RUNID'));
+        $this->gdClient = new Client();
+        $this->gdClient->setUserAgent('gooddata-writer-v3', getenv('KBC_RUNID'));
         if (isset($config['parameters']['project']['backendUrl'])) {
-            $gdClient->setApiUrl($config['parameters']['project']['backendUrl']);
+            $this->gdClient->setApiUrl($config['parameters']['project']['backendUrl']);
         }
-        $gdClient->login($config['parameters']['user']['login'], $config['parameters']['user']['#password']);
-        return $gdClient;
+        $this->gdClient->login($config['parameters']['user']['login'], $config['parameters']['user']['#password']);
+        return $this->gdClient;
     }
 }
